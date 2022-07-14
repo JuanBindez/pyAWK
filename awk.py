65966f7050953adc8c63aac21a6702b01fb53c24
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-07-13 23:45:32 -0300

    Update awk.py

--- awk.py
+++ awk.py
@@ -1,5 +1,7 @@
 '''
+"This project is licensed under the MIT License."
 Author: www.github.com/JuanBindez
+License: https://github.com/JuanBindez/AWK-PYTHON/blob/main/LICENSE
 Description: command awk
 Python Version: 3.10
 year: 2022
